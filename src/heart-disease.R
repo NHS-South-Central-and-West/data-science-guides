15b300d40702d9a6a7372b3779c5bc39f904bd17
Paul Johnson <paul.johnson50@nhs.net>
2023-07-22 01:19:07 +0100

    Remove heart disease data

--- src/heart-disease.R
+++
@@ -1,6 +0,0 @@
-# script to save sample datasets as .csv files
-
-heart_disease <- MLDataR::heartdisease |>
-  janitor::clean_names()
-
-readr::write_csv(heart_disease, here::here('data', 'heart_disease.csv'))
